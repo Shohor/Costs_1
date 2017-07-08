7d7ee9284e97568bae7751b7e425d172533a0394
Shokhor Ilya <shohor@mail.ru>
2017-07-08 18:10:53 +0200

    1

---
+++ src/main/java/de/shokhor/costs/model/BaseEntity.java
@@ -0,0 +1,8 @@
+package de.shokhor.costs.model;
+
+/**
+ * Created by user on 08.07.2017.
+ */
+public class BaseEntity {
+
+}
---
+++ src/main/java/de/shokhor/costs/model/Role.java
@@ -0,0 +1,7 @@
+package de.shokhor.costs.model;
+
+/**
+ * Created by user on 08.07.2017.
+ */
+public class Role {
+}
