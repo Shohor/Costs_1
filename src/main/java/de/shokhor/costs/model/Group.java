06f863c6cacfcfb1e7b40b5ab09b5e774cf63b37
Shokhor Ilya <shohor@mail.ru>
2017-07-10 11:54:59 +0200

    09.07.2017

--- src/main/java/de/shokhor/costs/model/Group.java
+++ src/main/java/de/shokhor/costs/model/Group.java
@@ -1,7 +1,39 @@
 package de.shokhor.costs.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 /**
  * Created by user on 08.07.2017.
  */
-public class Group {
+@Entity
+@Table(name = "groups")
+public class Group extends BaseEntity {
+
+    @Column(name = "Group")
+    @NotNull
+    private String group;
+
+    @OneToMany (mappedBy = "group")
+    private List<Cost> costs;
+
+    public Group() {
+    }
+
+    public Group(int id, String group) {
+        super(id);
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
 }
