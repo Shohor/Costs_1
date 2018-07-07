e2fd6dfb5900593cfeb62973ccac349d5d825b18
Shokhor Ilya <shohor@mail.ru>
2018-07-07 21:18:52 +0200

    13.10.2017

--- src/main/java/de/shokhor/costs/springMain.java
+++ src/main/java/de/shokhor/costs/springMain.java
@@ -2,15 +2,22 @@ package de.shokhor.costs;
 
 import de.shokhor.costs.repository.Jpa.JpaCostRepositoryImpl;
 import de.shokhor.costs.util.PasswordUtil;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class springMain {
     public static void main(String[] args) {
-        System.out.println(PasswordUtil.encode("Russia1984"));
-        System.out.println(PasswordUtil.isMatch("Shish1983","$2a$10$nLAIv6MfFCp2g1Y9iW.z0ubvrMzI2L0Sd.zimVj/REx2kQe1/A2xK"));
+        ConfigurableApplicationContext bf = new ClassPathXmlApplicationContext("spring/spring-db.xml");
+        System.out.println(Arrays.toString(bf.getBeanDefinitionNames()));
 
     }
 }
